86832ed57e02f570af8bcd945e2d97ce40265488
Jianjun99 <55260782+Jianjun99@users.noreply.github.com>
2026-08-29 12:18:33 -0400

    Scaffold ASP.NET Core 8 MVC app with Identity auth

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JobTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<JobApplication> JobApplications => Set<JobApplication>();
+}
